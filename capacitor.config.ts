ce7976c0cd686df9c84fcabe6735f8e6e2537801
YuriiKovalov <kovalovy76@gmail.com>
2022-12-14 21:36:47 +0200

    ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mr.studio',
+  appName: 'mr-spending',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
