7d00517df5f695259caffea5f70c8e9c3d8e160f
YuriiKovalov <kovalovy76@gmail.com>
2022-12-20 09:48:29 +0200

    init localization

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { AuthComponent } from './auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { TranslateModule } from '@ngx-translate/core';
 
 
 
