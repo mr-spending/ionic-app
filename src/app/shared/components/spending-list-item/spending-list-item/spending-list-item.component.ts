80ebdc7ca8a7e417b1e1355ba5336aaae9aaef45
YuriiKovalov <kovalovy76@gmail.com>
2023-01-30 15:12:57 +0200

    upd

--- src/app/shared/components/spending-list-item/spending-list-item/spending-list-item.component.ts
+++ src/app/shared/components/spending-list-item/spending-list-item/spending-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-spending-list-item',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./spending-list-item.component.scss'],
 })
 export class SpendingListItemComponent implements OnInit {
+  @Input() currency!: string | null;
 
   constructor() { }
 
