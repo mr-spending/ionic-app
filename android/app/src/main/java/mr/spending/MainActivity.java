b355ce3b526480fa30335d3da56abd8164e23178
ArtemNA <artem.nevmerzhytskyi@gmail.com>
2023-02-08 13:24:50 +0200

    update

---
+++ android/app/src/main/java/mr/spending/MainActivity.java
@@ -0,0 +1,5 @@
+package mr.spending;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,7 @@ export class AppComponent implements OnDestroy {
     this.initializeApp();
   }
 
+
   toggleDarkTheme(shouldAdd: boolean): void {
     document.body.classList.toggle('dark', shouldAdd);
   }
