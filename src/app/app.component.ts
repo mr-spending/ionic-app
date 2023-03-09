79d38aa609ed8f45f814afc3bbaa2ed4f7115944
yuriikovalov1993 <79081359+YuriiKovalov@users.noreply.github.com>
2023-03-09 18:41:00 +0200

    US-07031745 (#29)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Subscription, take } from 'rxjs';
+import { Observable, Subscription, take } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import { languageList } from './core/constants/languages.constants';
 import { UserActions } from './core/state/actions/user.actions';
 import { UserState } from './core/state/reducers/user.reducer';
+import { LoadingService } from './core/services/loading/loading.service';
 
 @Component({
   selector: 'app-root',
@@ -14,13 +15,16 @@ import { UserState } from './core/state/reducers/user.reducer';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnDestroy {
+  isAppLoading$: Observable<boolean>;
 
   subscription: Subscription = new Subscription();
   constructor(
     private translate: TranslateService,
     private angularFireAuth: AngularFireAuth,
     private userStore: Store<UserState>,
+    private loader: LoadingService,
   ) {
+    this.isAppLoading$ = this.loader.loading$;
     this.toggleDarkTheme(true);
     this.initializeApp();
   }
