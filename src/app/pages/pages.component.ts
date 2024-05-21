d520a29302e6460eeffa58c0509e66d434a6e65d
Kaktusryak <denis1243qwerty@gmail.com>
2024-05-21 14:55:35 +0300

    added change tabs on swipes left/right

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,9 +1,17 @@
-import { Component, OnDestroy } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { Subscription, take } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '@capacitor/app';
 import * as moment from 'moment';
+import { GestureController, GestureDetail } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 import { AuthService } from '../auth/services/auth.service';
 import { PageRoutesEnum } from '../core/enums/routing.enums';
@@ -12,12 +20,21 @@ import { BankAccountsActions } from '../core/state/actions/bank-accounts.actions
 import { SpendingActions } from '../core/state/actions/spending.actions';
 import { ViewPeriod } from '../core/enums/time.enum';
 
+export interface TabInterface {
+  route: string;
+  icon: string;
+  nameKey: string;
+}
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.scss'],
 })
 export class PagesComponent implements OnDestroy {
+  @ViewChild('tabsRef', { static: false, read: ElementRef })
+  tabsRef!: ElementRef;
+
   subscription: Subscription = new Subscription();
   tabs = [
     {
@@ -36,22 +53,75 @@ export class PagesComponent implements OnDestroy {
       nameKey: 'settings.title',
     },
   ];
+  selectedTab: string = this.tabs[0].route;
 
   constructor(
     private authService: AuthService,
     private translateService: TranslateService,
     private store: Store<AppState>,
+    private GestureCtrl: GestureController,
+    private router: Router
   ) {
-    this.store.dispatch(SpendingActions.updateStatTimePeriod({
-      payload: {
-        startDate: moment().startOf(ViewPeriod.Month).unix(),
-        endDate: moment().endOf(ViewPeriod.Month).unix()
-      }
-    }));
-    this.subscription.add(this.store.select(UserSelectors.selectUser)
-      .pipe(take(2))
-      .subscribe(value => value?.monoBankAccounts?.length && this.store.dispatch(BankAccountsActions.checkWebHook()))
+    this.store.dispatch(
+      SpendingActions.updateStatTimePeriod({
+        payload: {
+          startDate: moment().startOf(ViewPeriod.Month).unix(),
+          endDate: moment().endOf(ViewPeriod.Month).unix(),
+        },
+      })
     );
+    this.subscription.add(
+      this.store
+        .select(UserSelectors.selectUser)
+        .pipe(take(2))
+        .subscribe(
+          (value) =>
+            value?.monoBankAccounts?.length &&
+            this.store.dispatch(BankAccountsActions.checkWebHook())
+        )
+    );
+  }
+
+  setupSwipeGesture() {
+    const gesture = this.GestureCtrl.create({
+      el: this.tabsRef.nativeElement,
+      gestureName: 'swipe',
+      onEnd: (ev) => this.onSwipe(ev),
+    });
+    gesture.enable(true);
+  }
+
+  onSwipe(ev: GestureDetail) {
+    if (ev.deltaX > 100) {
+      this.swipePrev();
+    } else if (ev.deltaX < -100) {
+      this.swipeNext();
+    }
+  }
+
+  swipeNext() {
+    const currentIndex = this.tabs.findIndex(
+      (tab) => tab.route === this.selectedTab
+    );
+    const nextIndex = (currentIndex + 1) % this.tabs.length;
+    this.selectTab(this.tabs[nextIndex]);
+  }
+
+  swipePrev() {
+    const currentIndex = this.tabs.findIndex(
+      (tab) => tab.route === this.selectedTab
+    );
+    const prevIndex = (currentIndex - 1 + this.tabs.length) % this.tabs.length;
+    this.selectTab(this.tabs[prevIndex]);
+  }
+
+  selectTab(tab: TabInterface) {
+    this.selectedTab = tab.route;
+    this.router.navigateByUrl('pages/' + this.selectedTab);
+  }
+
+  ngAfterViewInit() {
+    this.setupSwipeGesture();
   }
 
   ngOnDestroy() {
